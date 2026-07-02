09dcc3b17fcdef694667250e5dfcd02efdf1ee7c
Mr_Black <you@example.com>
2026-07-02 03:53:25 +0200

    Commit 6 of the week

---
+++ weekly_progress/Month1_Week_1/mario.py
@@ -0,0 +1,51 @@
+'''
+print("#")
+print("#")
+print("#")
+print("\n\n")
+
+#Let's optimize
+for _ in range(3):
+    print("#\n", end="")
+
+
+def main():
+    print_column(3)
+    
+    
+def print_column(height):
+    for _ in range(height):
+        print("#")
+    print("#\n" * height, end="")
+
+main()
+
+
+def main():
+    print_row(4)
+
+
+def print_row(width):
+    print("?" * width)
+
+main()
+'''
+
+def main():
+    print_square(3)
+
+#For each row in square
+def print_square(size):
+
+    #For each row in square
+    for i in range(size):
+
+        #For each brick in row
+        for j in range(size):
+
+            #Print a brick
+            print("#", end="")
+
+        print()
+
+main()
